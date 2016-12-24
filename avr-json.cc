0743d27c0072c3439a65aae3b377479e3c92eac7
Kevin Fox <kevin.j.fox@gmail.com>
2016-12-24 15:12:25 -0500

    Updating code to support input and output in json format.
    - input from serial port parsed as json

--- avr-json.cc
+++ avr-json.cc
@@ -1,12 +1,19 @@
 //avr-json Proof of Concept tesing using ArduinoJson for Wireless Sensor Network Comminication
+/*
+Change Log
+2016-12-23: Test JSON Output
+2016-12-24: Add Serial input
 
+*/
 
 #include <ArduinoJson.h>
 #define SLEEP 5000
 // Create 200B buffer for json object
 StaticJsonBuffer<200> jsonBuffer;
 // Create json object
-JsonObject& json = jsonBuffer.createObject();
+JsonObject& json_out = jsonBuffer.createObject();
+
+char SerialBuffer[200];
 
 void setup() {
 	Serial.begin(115200);
@@ -14,18 +21,29 @@ void setup() {
 		//Wait for serial port to initialize
 	}
 	// Reserve memory for json buffer
-	json["node"] = "1";
+	json_out["node"] = "1";
 }
 
 void loop() {
 	bool MsgSent = false;
-	if ( millis() % SLEEP == 0  && ! MsgSent ) {
-		json["millis"] = millis();
-		json.printTo(Serial);
-		Serial.println();
-		delay(1);
-		MsgSent = true;
+	// Test if serial buffer has input
+	if ( Serial.available() > 0 ) {
+		// Read incomnig byte until a trailing '}' character is detected.
+		Serial.readBytesUntil('}', SerialBuffer, 199);
+		// Parse character buffer from serial port as json
+		JsonObject& json_in = jsonBuffer.parseObject(SerialBuffer);
+		
+
 	} else {
-		MsgSent = false;
-	}
+		// Send normal output 
+		if ( millis() % SLEEP == 0  && ! MsgSent ) {
+			json_out["millis"] = millis();
+			json_out.printTo(Serial);
+			Serial.println();
+			delay(1);
+			MsgSent = true;
+		} else {
+			MsgSent = false;
+		}
+	}	
 }
